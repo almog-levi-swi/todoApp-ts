d8b672b9049dca2fe5d6a8d2b314307224d29f87
almog-levi-swi <levi.almog@solarwinds.com>
2022-02-24 15:11:03 +0200

    first commit with tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('test todo list app', () => {
+
+  test('label and placeholder should be in the document', () => {
+    render(<App />);
+
+    expect(screen.getByText('New Todo Text')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Add new task/)).toBeInTheDocument();   
+    
+  });
+
+  test('should add new task', async () => {
+    render(<App/>);
+
+    expect(screen.queryByText('learn to work with docker')).toBeNull();
+
+    // screen.debug();
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'learn to work with docker' }
+    });
+    const btns = screen.getAllByRole<HTMLButtonElement>('button');
+    fireEvent.click(btns[0]);
+    // screen.debug();
+
+    expect(screen.queryByText('learn to work with docker')).toBeInTheDocument();
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 import './App.css';
 
-function App() {
+interface Todo {
+  id: string,
+  text: string
+}
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([{id: 'ff1', text: 'finish course'}, {id: 'ff2', text: 'clean home'}]);
+
+
+  const onAddTodoHandler = (text: string) => {
+    console.log(text);
+    setTodos(prevTodos => [...prevTodos, {id: Math.random().toString(), text: text}]);
+  }
+
+  const deleteTodo = (id: string) => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos([...newTodos]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodo onAddTodo={onAddTodoHandler} />
+      <TodoList items={todos} deleteTodo={deleteTodo}/>
     </div>
   );
 }
